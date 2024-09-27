d218d9ad8a90904550da7807743f03961cf3ac58
Akhil Badam <120327949+akhil242628@users.noreply.github.com>
2024-09-27 22:40:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function manipulateStyles() {
+   document.getElementById("headingElement").textContent = "HAPPY BIRTHDAY PRASANNA";
+   document.getElementById("headingElement").style.color = "#13c1e8";
+ };
